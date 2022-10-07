11427a1eb2ec310f0361d70c49589fc9d6df94a4
admin <admin@judgesteels-Mac-Pro.local>
2022-10-07 10:15:51 -0400

    update constants

--- scripts/terra/src/constants.ts
+++ scripts/terra/src/constants.ts
@@ -1,10 +1,21 @@
 import "dotenv/config";
 
-export const auto = "terra1679n3rthcvghet6tc29ugqtn5f3axp0lvceshq0c0r482pps3u4qggfyax";
-export const registry = "terra14d2php0g2rn3a49qk3vgpjj6vck7j9z09nvsd0ww8zpk5nfa9s0szsvvug";
-export const wrapperAstroport = "terra1v2dc3mefcnuqra0k2le7w6kk3yz043l92rcvd86k5tgd6ryvm2ps3q78gs";
-
 export const STAN_STAKE = 1000000;
 
-export const registryCodeId = 1437;
-export const wrapperAstroportCodeId = 1420;
+// phoenix mainnet
+
+export const auto = "terra1jh37p2akmwprfwr2235kjyt3wuz0s2sfenhtytg9cajsxnxfvdasnaurdp";
+export const registry = "terra1upye9vg8j539ndtf2ysa3e3sf58nt3l0y89ekkuq6xkgnn0javusm4aayt";
+export const wrapperAstroport = "terra19camas6u6pwyw3pedz0f0hge9cphgczz83s6fxwklwy7g4ju4vesrdaw89";
+
+export const registryCodeId = 519;
+export const wrapperAstroportCodeId = 520;
+
+// pisco testnet
+
+// export const auto = "terra1679n3rthcvghet6tc29ugqtn5f3axp0lvceshq0c0r482pps3u4qggfyax";
+// export const registry = "terra14d2php0g2rn3a49qk3vgpjj6vck7j9z09nvsd0ww8zpk5nfa9s0szsvvug";
+// export const wrapperAstroport = "terra1v2dc3mefcnuqra0k2le7w6kk3yz043l92rcvd86k5tgd6ryvm2ps3q78gs";
+
+// export const registryCodeId = 1437;
+// export const wrapperAstroportCodeId = 1420;
