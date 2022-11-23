c4d831cd0e4b5d6825407c2993da174acbf607a4
autonomydev <arcpinus@gmail.com>
2022-11-23 08:09:47 -0500

    add example request creation script

--- scripts/osmosis/src/execute.ts
+++ scripts/osmosis/src/execute.ts
@@ -1,31 +1,46 @@
 import { coin } from "@cosmjs/proto-signing";
 import { getRegistryClient } from "./connection";
 import "./constants";
-
-// const swap = {
-//   user: "osmo1phaxpevm5wecex2jyaqty2a4v02qj7qmlmzk5a",
-//   amount: "1000",
-//   min_output: "0",
-//   max_output: "3275571",
-//   first: {
-//     pool_id: "1",
-//     denom_in:
-//       "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
-//     denom_out: "uosmo",
-//   },
-//   route: [
-//     // {
-//     //   pool_id: 2,
-//     //   denom_out: "uion",
-//     // },
-//   ],
-// };
+import { toBase64 } from "./util";
 
 async function main() {
   const client = await getRegistryClient();
-  await client.stakeDenom({ numStakes: 1 }, "auto", undefined, [
-    coin(10000, "uosmo"),
-  ]);
+
+  const swap = {
+    user: "osmo1phaxpevm5wecex2jyaqty2a4v02qj7qmlmzk5a",
+    amount: "10000000",
+    min_output: "1",
+    max_output: "18446744073709551615",
+    first: {
+      pool_id: "1",
+      denom_in: "uosmo",
+      denom_out:
+        "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
+    },
+    route: [],
+  };
+
+  await client.createRequest(
+    {
+      requestInfo: {
+        target:
+          "osmo1dwpdh2clk7c8csf9ql2xj36336xsryyg4j7622jhaert9htp48gsh8u9ve",
+        msg: toBase64(swap),
+        input_asset: {
+          info: {
+            native_token: {
+              denom: "uosmo",
+            },
+          },
+          amount: "10000000",
+        },
+        is_recurring: false,
+      },
+    },
+    "auto",
+    undefined,
+    [coin("11000000", "uosmo")]
+  );
 }
 
 main().catch(console.error);
